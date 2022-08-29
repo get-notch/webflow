d2c44261f21529657799f1739bad4fafdad6bd31
roee-notch <roee@get-notch.com>
2022-08-29 15:24:29 +0300

    First working branch

--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import esbuild from 'esbuild';
 import fs from 'fs';
+import http from 'http';
 import path from 'path';
 
 function getAllFilesInDirectory(directory) {
@@ -48,11 +49,51 @@ else {
     .serve(
       {
         servedir: buildDirectory,
-        port: 3000,
+        port: 3001,
       },
       defaultSettings
     )
-    .then((server) => {
-      console.log(`Serving at http://localhost:${server.port}`);
+    // We create a server that forwards requests to esbuild's server in order to add CORS headers.
+    // This allows us to serve this content using ngrok without hitting CORS errors when trying to fetch the
+    // files from Webflow.
+    // All of this allows us to run ngrok on a laptop and show somebody else our work in progress while developing.
+    .then((result) => {
+      const { host, port } = result;
+      http
+        .createServer((req, res) => {
+          const options = {
+            hostname: host,
+            port: port,
+            path: req.url,
+            method: req.method === 'OPTIONS' ? 'GET' : req.method,
+            headers: req.headers,
+          };
+
+          const proxyReq = http.request(options, (proxyRes) => {
+            if (proxyRes.statusCode === 404) {
+              res.writeHead(404, { 'Content-Type': 'text/html' });
+              res.end('<h1>A custom 404 page</h1>');
+              return;
+            }
+
+            const additionalHeaders = {
+              'Access-Control-Allow-Origin': 'https://get-notch.webflow.io',
+              'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+              'Access-Control-Allow-Headers': '*',
+              'Access-Control-Max-Age': '60',
+            };
+            const headers = {
+              ...proxyRes.headers,
+              ...additionalHeaders,
+            };
+            res.writeHead(proxyRes.statusCode, headers);
+            proxyRes.pipe(res, { end: true });
+          });
+
+          req.pipe(proxyReq, { end: true });
+        })
+        .listen(3000, () => {
+          console.log('listening on port ' + 3000);
+        });
     });
 }
---
+++ src/pages/homepage.js
@@ -0,0 +1,139 @@
+//Homepage Accordion
+
+function isWidthUnderPx(threshold) {
+  return window.matchMedia(`(max-width: ${threshold}px)`).matches;
+}
+
+const elements = ['.is--creators', '.is--shopers', '.is--nfts', '.is--gamers'];
+const openedSelectors = ['shopers-opened', 'nfts-opened', 'gamers-opened', 'creators-opened'];
+
+function replaceOpenedClass(classToOpen) {
+  for (const openClassName of openedSelectors) {
+    $('.vertical-text').removeClass(openClassName);
+  }
+  $('.vertical-text').addClass(classToOpen);
+  for (const element of elements) {
+    const selectedElement = $(element);
+    for (const openClassName of openedSelectors) {
+      selectedElement.removeClass(openClassName);
+    }
+    selectedElement.addClass(classToOpen);
+  }
+}
+
+//Creators
+$('.is--creators').on('click', function () {
+  replaceOpenedClass('creators-opened');
+});
+
+// Shoppers
+$('.is--shopers').on('click', function () {
+  replaceOpenedClass('shopers-opened');
+});
+
+//gamers
+$('.is--gamers').on('click', function () {
+  replaceOpenedClass('gamers-opened');
+});
+
+//NFTs
+$('.is--nfts').on('click', function () {
+  replaceOpenedClass('nfts-opened');
+});
+
+// Bubbles Testimonials Splide
+let object = {
+  value: 1,
+};
+
+let tl = gsap.timeline({ repeat: -1 });
+tl.fromTo(
+  '.bubbles_marquee_track',
+  {
+    xPercent: 0,
+  },
+  {
+    xPercent: -50,
+    duration: 100,
+    ease: 'none',
+  }
+);
+
+$('.bubbles_marquee_track').on('mouseenter', function () {
+  gsap.fromTo(
+    object,
+    {
+      value: 1,
+    },
+    {
+      value: 0,
+      duration: 0.5,
+      onUpdate: () => {
+        tl.timeScale(object.value);
+      },
+    }
+  );
+});
+
+$('.bubbles_marquee_track').on('mouseleave', function () {
+  gsap.fromTo(
+    object,
+    {
+      value: 0,
+    },
+    {
+      value: 1,
+      duration: 0.2,
+      onUpdate: () => {
+        tl.timeScale(object.value);
+      },
+    }
+  );
+});
+
+//Hover in to dim the other bubbles
+$('.bubble-collection-item').on('mouseenter', function () {
+  $(this).siblings('.bubble-collection-item').css('opacity', '50%');
+});
+
+//Hover out to dim the other bubbles
+$('.bubble-collection-item').on('mouseleave', function () {
+  $(this).siblings('.bubble-collection-item').css('opacity', '100%');
+});
+
+// new Splide('.bubbles_wrapper', {
+//   autoScroll: {
+//     speed: 2,
+//     pauseOnHover: false,
+//   },
+//   // direction: 'ltr',
+//   width: '100vw',
+//   perPage: 2,
+//   perMove: 1,
+//   focus: 'center', // 0 = left and 'center' = center
+//   type: 'loop', // 'loop' or 'slide'
+//   gap: '2.5em', // space between slides
+//   arrows: false, // 'slider' or false
+//   pagination: false, // 'slider' or false
+//   speed: 600, // transition speed in miliseconds
+//   drag: false,
+//   dragAngleThreshold: 30, // default is 30
+//   autoWidth: false, // for cards with differing widths
+//   rewind: false, // go back to beginning when reach end
+//   rewindSpeed: 400,
+//   waitForTransition: false,
+//   updateOnMove: true,
+//   trimSpace: false, // true removes empty space from end of list
+//   // breakpoints: {
+//   //   640: {
+//   //     // height: '6rem',
+//   //   },
+//   // },
+// }).mount(window.splide.Extensions);
+//
+// $('.right_arrow').on('click', function () {
+//   $('.splide__arrow--next').click();
+// });
+// $('.left_arrow').on('click', function () {
+//   $('.splide__arrow--prev').click();
+// });
